a4c0a1bd950db28df1a4cfa0f4d9181c2bdecdfd
Jaret Peerson <contact@jaretpeerson.com>
2026-02-14 20:33:07 -0500

    Installed Astro sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from "astro/config"
+import sitemap from "@astrojs/sitemap"
 
 export default defineConfig({
+  site: "https://toggle.supply",
+
   devToolbar: {
     enabled: false,
   },
+
   vite: {
     resolve: {
       alias: {
@@ -11,4 +15,6 @@ export default defineConfig({
       },
     },
   },
+
+  integrations: [sitemap()],
 })

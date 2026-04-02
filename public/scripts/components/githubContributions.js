3763735741ce42b301550aa6d5f326087ee1020c
Jaret Peerson <contact@jaretpeerson.com>
2026-04-01 23:24:15 -0400

    Final optimizations

--- public/scripts/components/githubContributions.js
+++ public/scripts/components/githubContributions.js
@@ -1,5 +1,4 @@
-const API_URL =
-  "https://github-contributions-api.jogruber.de/v4/andrewgolovanov?y=last"
+const API_URL = "https://github-contributions-api.jogruber.de/v4/HamedMP?y=last"
 // To switch users, replace "andrewgolovanov" in the URL above with the desired GitHub username
 
 const MOBILE_BREAKPOINT = 650
